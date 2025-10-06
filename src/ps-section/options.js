8dff6b5f4c871607dc34c6b1899651c967986b68
suzan <asadsuzan7@gmail.com>
2025-10-06 12:35:12 +0600

    fix: section padding and width issues

---
+++ src/ps-section/options.js
@@ -0,0 +1,51 @@
+export const gradients = [
+  {
+    name: "gradient-1",
+    slug: "gradient-1",
+    gradient: "linear-gradient(to bottom right, #312e81, #581c87, #9d174d)",
+  },
+
+  {
+    name: "gradient-2",
+    slug: "gradient-2",
+    gradient: "linear-gradient(to bottom right, #111827, #000, #1e3a8a)",
+  },
+  {
+    name: "gradient-3",
+    slug: "gradient-3",
+    gradient: "linear-gradient(to bottom right, #5b21b6, #6b21a8, #312e81)",
+  },
+  {
+    name: "gradient-4",
+    slug: "gradient-4",
+    gradient: "linear-gradient(to bottom right, #0f172a, #111827, #000000)",
+  },
+];
+
+export const overlayColor = [
+  {
+    name: "overlay-1",
+    slug: "overlay-1",
+    gradient:
+      "linear-gradient(to right, rgba(37, 99, 235, 0.2), rgba(147, 51, 234, 0.2))",
+  },
+
+  {
+    name: "overlay-2",
+    slug: "overlay-2",
+    gradient:
+      "linear-gradient(to right, rgba(37, 99, 235, 0.1), rgba(147, 51, 234, 0.1))",
+  },
+  {
+    name: "overlay-3",
+    slug: "overlay-3",
+    gradient:
+      "inear-gradient(to right, rgba(124, 58, 237, 0.1), rgba(79, 70, 229, 0.1))",
+  },
+  {
+    name: "overlay-4",
+    slug: "overlay-4",
+    gradient:
+      "inear-gradient(to right, rgba(22, 163, 74, .10), rgba(37, 99, 235, .10))",
+  },
+];
